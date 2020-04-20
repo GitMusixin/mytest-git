ed5b08f8f9a3d9c24de1451b1afd5b27f6f5ac8c
root <1254020980@qq.com>
2020-04-20 11:42:55 +0800

    v2.2

--- src/main/com/enjoy/action/chain/ChainCartClient.java
+++ src/main/com/enjoy/action/chain/ChainCartClient.java
@@ -33,7 +33,7 @@ public class ChainCartClient {
 
         ShoppingCart cart = new OtherPayShopping(products);
 
-        //注入优惠 ---master
+        //注入优
         cart.setDiscount(multyDiscount);
 
         cart.submitOrder();
